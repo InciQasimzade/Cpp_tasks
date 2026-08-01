d0930e83e21e306e3ef1c85ada8f350162362528
InciQasimzade <qasimzade.inci2011@gmail.com>
2026-08-01 11:36:13 +0400

    Create while.cpp

---
+++ while.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int i = 3;
+    while (i < 10){
+        i += 1;
+        if(i % 2 == 1){
+            cout<< i<< endl;
+        }
+    }
+    
+    return 0;
+}
