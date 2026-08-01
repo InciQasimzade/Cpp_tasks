d92d689f78f0cfb580675686cadcecf8689f1bbb
InciQasimzade <qasimzade.inci2011@gmail.com>
2026-08-01 11:29:10 +0400

    Create inputs.cpp

---
+++ inputs.cpp
@@ -0,0 +1,56 @@
+
+#include <iostream>
+using namespace std;
+//     int number = 23;
+//     cout<< number<< endl;
+    
+//     int number2
+//     cout<<"Enter a number: ";
+//     cin>> number2;
+//     cout<<number2<<endl;
+    
+//     string word = "hello";
+//     cout<<word<<endl;
+    
+//     string word2
+//     cout<<"Enter a word: ";
+//     cin>> word2;
+//     cout<<word2<<endl;
+    
+//     char anotherword = 'e';
+//     cout<< anotherword<<endl;
+    
+//     char anotherword2;
+//     cout<<"Enter a word: ";
+//     cin>> anotherword2;
+//     cout<<anotherword2<<endl;
+    
+//     float number3 = 4.12;
+//     cout<<number3<<endl;
+    
+//     float number4;
+//     cout<<"Enter a float num: ";
+//     cin>> number4;
+//     cout<<number4<<endl
+
+int main(){
+    string name;
+    cout<<"Enter your name: "<<endl;
+    cin>>name;
+    
+    string age;
+    cout<<"Enter your age: "<<endl;
+    cin>>age;
+    
+    string surname;
+    cout<<"Enter your surname: "<<endl;
+    cin>>surname;
+    
+    string height;
+    cout<< "enter your height: "<<endl;
+    cin>>height;
+    
+    cout<<"Your name is "<< name << ", " << "your surname is " << surname << ", " << "your age is " << age << ", " << "your height is " << height;
+
+    return 0;
+}
