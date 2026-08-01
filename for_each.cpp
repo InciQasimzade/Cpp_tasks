02bc233e782182c2083a8b1c9c66cfce3045cf7f
InciQasimzade <qasimzade.inci2011@gmail.com>
2026-08-01 11:32:26 +0400

    Create for_each.cpp

---
+++ for_each.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+// for i in name(in python)
+int main()
+{
+    string name = "Inci";
+    for (char c : name){
+        cout<<c<<endl;
+    }
+    
+    return 0;
+}
