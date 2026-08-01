84e5952bad1fa702d17cf81840146fc7933f3ac6
InciQasimzade <qasimzade.inci2011@gmail.com>
2026-08-01 11:31:32 +0400

    Create for_i_in_range.cpp

---
+++ for_i_in_range.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // for(int i = 1; i < 20; i++){
+    //     cout<<i<<" ";
+    // }
+    
+    string sentence = "hello everyone";
+    for(char letter : sentence){
+        cout<<letter<<" "
+    }
+    
+    return 0;
+}
